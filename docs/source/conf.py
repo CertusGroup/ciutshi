23514f5d7b92d73db31180a74dd4986544a4c49b
--help <thejoeblankenship@protonmail.com>
2022-08-11 09:55:25 -0400

    index - conf - update release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Certus Core'
 author = "<a href='https://certusgroup.org' target='_blank'>Certus Core</a>"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/source/index.md
+++ docs/source/index.md
@@ -1,6 +1,6 @@
 # ciuTshi
 
-Release v0.1.0
+Release v0.2.0
 
 ## The Adaptable Data Operations Framework
 
